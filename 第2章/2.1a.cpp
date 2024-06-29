e7b221b3d81d13ab7a2cd83772f5b4c91641390d
ZhengkaiXu <695378091@qq.com>
2024-06-30 02:54:13 +0800

    first commmit

---
+++ 第2章/2.1a.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ZhengkaiXu on 2024/6/30.
+//
+int main() {
+    int a; // 用关键字int指定变量a的类型
+    float b; // 用关键字float指定变量b的类型
+    char c; // 用关键字char指定变量c的类型
+    a = 1; // 为int型变量a赋值整型常量1
+    b = 2.5; // 为float型变量b赋值实型常量2.5
+    c = 'A'; // 为char型变量c赋值字符型常量'A'
+    return 0;
+}
