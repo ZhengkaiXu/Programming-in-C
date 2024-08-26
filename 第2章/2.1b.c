b9d35e12e528d0d97d738d445c6cd6007f367670
ZhengkaiXu <695378091@qq.com>
2024-08-26 23:19:27 +0800

    2.1b

---
+++ 第2章/2.1b.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+    int a = 1;
+    float b = 2.5;
+    char c = 'A';
+    printf("a = %d \n", a);        // 按整型格式输出变量a的值
+    printf("b = %f \n", b);        // 按实型格式输出变量b的值
+    printf("c = %c \n", c);        // 按字符型格式输出变量c的值
+    printf("End of program\n");    // 输出一个字符串
+}
