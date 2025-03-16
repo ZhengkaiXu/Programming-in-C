606c77dd81843bd7978fcc5a803d7b61ed702fbf
ZhengkaiXu <695378091@qq.com>
2025-03-16 20:30:14 +0800

    7.2

---
+++ ch7/7.2.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+long Fact(int n);
+int main(void) {
+    int m;
+    long ret;
+    printf("Input m:");
+    scanf("%d", &m);
+    ret = Fact(m);
+    printf("%d! = %ld\n", m, ret);
+    return 0;
+}
+
+// 函数功能：用迭代法计算n！
+long Fact(int n) {
+    int i;
+    long result = 1;
+    if (n < 0) { // 增加对函数入口参数合法性的检查
+        printf("Input data error!\n");
+    } else {
+        for (i = 2; i <= n; i++) {
+            return result *= i;
+        }
+        return result;
+    }
+}
